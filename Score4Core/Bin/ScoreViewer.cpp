3f82c6ce5a5ba8b976623138593311b75e014aa6
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2017-09-16 14:40:23 +0900

    実行バイナリの生成準備

---
+++ Score4Core/Bin/ScoreViewer.cpp
@@ -0,0 +1,26 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  The Score4 Core Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2017, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      ビューワープログラム。
+**
+**      @file       Bin/ScoreView.cpp
+**/
+
+#include    "Score4Core/Common/Score4CoreSettings.h"
+
+#include    <iostream>
+
+using   namespace   SCORE4_CORE_NAMESPACE;
+
+int  main(int argc, char * argv[])
+{
+    return ( 0 );
+}
