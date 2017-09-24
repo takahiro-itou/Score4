e374e0065f536ee7e6b0dce54f86786ef7d5fdd2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2017-09-24 20:35:45 +0900

    ヘッダファイルを追加

---
+++ Score4Core/Include/Score4Core/Common/Score4Types.h
@@ -0,0 +1,33 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  The Score4 Core Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2017, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Type Definitions.
+**
+**      @file       Common/Score4Types.h
+**/
+
+#if !defined( SCORE4CORE_COMMON_INCLUDED_SCORE4_TYPES_H )
+#    define   SCORE4CORE_COMMON_INCLUDED_SCORE4_TYPES_H
+
+#include    "Score4CoreSettings.h"
+
+SCORE4_CORE_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    Type Definitions.
+//
+
+}   //  End of namespace  Common
+SCORE4_CORE_NAMESPACE_END
+
+#endif
