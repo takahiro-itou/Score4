94d180fa03ce2745efe269ac595d43553109af86
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2017-09-24 20:28:51 +0900

    クラスを新規作成

---
+++ Score4Core/Include/Score4Core/Common/ScoreDocument.h
@@ -0,0 +1,90 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  The Score4 Core Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2017, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of ScoreDocument class.
+**
+**      @file       Common/ScoreDocument.h
+**/
+
+#if !defined( SCORE4CORE_COMMON_INCLUDED_SCORE_DOCUMENT_H )
+#    define   SCORE4CORE_COMMON_INCLUDED_SCORE_DOCUMENT_H
+
+#include    "Score4CoreSettings.h"
+
+SCORE4_CORE_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    ScoreDocument  class.
+//
+
+class  ScoreDocument
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   ScoreDocumentTest;
+};
+
+}   //  End of namespace  Common
+SCORE4_CORE_NAMESPACE_END
+
+#endif
---
+++ Score4Core/Lib/Common/ScoreDocument.cpp
@@ -0,0 +1,68 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  The Score4 Core Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2017, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of ScoreDocument class.
+**
+**      @file       Common/ScoreDocument.cpp
+**/
+
+#include    "Score4Core/Common/ScoreDocument.h"
+
+SCORE4_CORE_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    ScoreDocument  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Common
+SCORE4_CORE_NAMESPACE_END
