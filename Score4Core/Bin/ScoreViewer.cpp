ed1e9d28e230697339321a4a1b2d242e51aae353
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2018-05-02 15:18:17 +0900

    スコアを集計して表示する機能を追加

--- Score4Core/Bin/ScoreViewer.cpp
+++ Score4Core/Bin/ScoreViewer.cpp
@@ -18,10 +18,31 @@
 
 #include    "Score4Core/Common/DocumentFile.h"
 #include    "Score4Core/Common/ScoreDocument.h"
+#include    "Score4Core/Common/ScoreInterface.h"
 
 #include    <iostream>
 
 using   namespace   SCORE4_CORE_NAMESPACE;
+
+void  writeCountedScores(
+        const   Common::CountedScores & cs,
+        std::ostream                  & outStr)
+{
+    outStr  <<  "Wons  : "  <<  (cs.numWons[Common::FILTER_ALL_GAMES])
+            <<  " (H: "     <<  (cs.numWons[Common::FILTER_HOME_GAMES])
+            <<  ", A: "     <<  (cs.numWons[Common::FILTER_AWAY_GAMES])
+            <<  ")\n"
+            <<  "Losts : "  <<  (cs.numLost[Common::FILTER_ALL_GAMES])
+            <<  " (H: "     <<  (cs.numLost[Common::FILTER_HOME_GAMES])
+            <<  ", A: "     <<  (cs.numLost[Common::FILTER_AWAY_GAMES])
+            <<  ")\n"
+            <<  "Draws : "  <<  (cs.numDraw[Common::FILTER_ALL_GAMES])
+            <<  " (H: "     <<  (cs.numDraw[Common::FILTER_HOME_GAMES])
+            <<  ", A: "     <<  (cs.numDraw[Common::FILTER_AWAY_GAMES])
+            <<  ")\n";
+    outStr  <<  std::endl;
+}
+
 
 int  main(int argc, char * argv[])
 {
@@ -40,6 +61,16 @@ int  main(int argc, char * argv[])
     retErr  = docFile.saveToTextStream(objDoc, std::cout);
     if ( retErr != Common::ERR_SUCCESS ) {
         return ( retErr );
+    }
+
+    std::vector<Common::CountedScores>  cs;
+    retErr  = objDoc.countScores(-1, cs);
+    std::cerr   <<  (cs.size())
+                <<  " Teams Found"  <<  std::endl;
+    for ( size_t i = 0; i < cs.size(); ++ i ) {
+        std::cerr   <<  "Team[" <<  i   <<  "] "
+                    <<  objDoc.getTeamInfo(i).teamName;
+        writeCountedScores(cs[i], std::cerr);
     }
 
     retErr  = docFile.saveToBinaryFile(objDoc, "dummy.bin");
