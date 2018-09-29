424cd2add93e5e240cc0759d9c2e09aef1ac4278
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2018-09-29 20:17:40 +0900

    クラスを新規作成

---
+++ Score4Core/Include/Score4Core/Common/TextParser.h
@@ -0,0 +1,95 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  The Score4 Core Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2018, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of TextParser class.
+**
+**      @file       Common/TextParser.h
+**/
+
+#if !defined( SCORE4CORE_COMMON_INCLUDED_TEXT_PARSER_H )
+#    define   SCORE4CORE_COMMON_INCLUDED_TEXT_PARSER_H
+
+SCORE4_CORE_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    TextParser  class.
+//
+
+class  TextParser
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   TextParserTest;
+};
+
+}   //  End of namespace  Common
+SCORE4_CORE_NAMESPACE_END
+
+#endif
+
+
---
+++ Score4Core/Lib/Common/TextParser.cpp
@@ -0,0 +1,74 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  The Score4 Core Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2018, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of TextParser class.
+**
+**      @file       Common/TextParser.cpp
+**/
+
+#include    "Score4Core/Common/TextParser.h"
+
+
+SCORE4_CORE_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    TextParser  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Common
+SCORE4_CORE_NAMESPACE_END
