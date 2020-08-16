eabfc8fc362b938ceee7810988005c99cc765495
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-08-16 20:19:01 +0900

    ヘッダファイルを追加

---
+++ Core/Include/Score4Core/Downloader/HtmlDownloader.h
@@ -0,0 +1,13 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  The Score4 Core Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
