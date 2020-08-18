8fb52bc1d81f2c9306107af469b940d3f1d5f194
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-08-18 21:32:42 +0900

    ファイルを別ディレクトリに移動

--- Core/Lib/Document/DocumentFile.cpp
+++ Core/Lib/Document/DocumentFile.cpp
@@ -3,15 +3,19 @@
 **                                                                      **
 **                  ---  The Score4 Core Library.  ---                  **
 **                                                                      **
-**          Copyright (C), 2017-2018, Takahiro Itou                     **
+**          Copyright (C), 2017-2020, Takahiro Itou                     **
 **          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
 **                                                                      **
 *************************************************************************/
 
 /**
 **      An Implementation of DocumentFile class.
 **
-**      @file       Common/DocumentFile.cpp
+**      @file       Document/DocumentFile.cpp
 **/
 
 #include    "Score4Core/Common/DocumentFile.h"
