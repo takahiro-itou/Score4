e28398a5ade8980db6ab184f4942b011c3f507ab
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-10-05 22:43:40 +0900

    クラスを新規作成

---
+++ Core/Include/Score4Core/Common/ProcessManager.h
@@ -0,0 +1,114 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  The Score4 Core Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of ProcessManager class.
+**
+**      @file       Common/ProcessManager.h
+**/
+
+#if !defined( SCORE4CORE_COMMON_INCLUDED_PROCESS_MANAGER_H )
+#    define   SCORE4CORE_COMMON_INCLUDED_PROCESS_MANAGER_H
+
+#include    "Score4Types.h"
+
+SCORE4_CORE_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    ProcessManager  class.
+//
+
+class  ProcessManager
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    ProcessManager();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~ProcessManager();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   ProcessManagerTest;
+};
+
+}   //  End of namespace  Common
+SCORE4_CORE_NAMESPACE_END
+
+#endif
