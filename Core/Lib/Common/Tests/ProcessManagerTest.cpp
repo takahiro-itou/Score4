85a4da29ed974032eebf2025eab6a4051599efe9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-10-05 22:55:37 +0900

    単体テストクラスを新規作成

---
+++ Core/Lib/Common/Tests/ProcessManagerTest.cpp
@@ -0,0 +1,75 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  The Score4 Core Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Test Case 'ProcessManager'.
+**
+**      @file       Common/Tests/ProcessManager.cpp
+**/
+
+#include    "TestDriver.h"
+#include    "Score4Core/Common/ProcessManager.h"
+
+SCORE4_CORE_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    ProcessManagerTest  class.
+//
+/**
+**    クラス ProcessManager の単体テスト。
+**/
+
+class  ProcessManagerTest : public  TestFixture
+{
+    CPPUNIT_TEST_SUITE(ProcessManagerTest);
+    CPPUNIT_TEST(testProcessManager);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    virtual  void   setUp()     override    { }
+    virtual  void   tearDown()  override    { }
+
+private:
+    void  testProcessManager();
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( ProcessManagerTest );
+
+//========================================================================
+//
+//    Tests.
+//
+
+void  ProcessManagerTest::testProcessManager()
+{
+    ProcessManager      testee;
+
+    return;
+}
+
+}   //  End of namespace  Common
+SCORE4_CORE_NAMESPACE_END
+
+//========================================================================
+//
+//    エントリポイント。
+//
+
+int  main(int argc, char * argv[])
+{
+    return ( executeCppUnitTests(argc, argv) );
+}
