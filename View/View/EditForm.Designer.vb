35db95763c51cf6a46d545536dd891c533d3a805
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-08-28 09:51:19 +0900

    空のフォームをデザイン

---
+++ View/View/EditForm.Designer.vb
@@ -0,0 +1,18 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EditForm
+    Inherits System.Windows.Forms.Form
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    Private components As System.ComponentModel.IContainer
+
+End Class
---
+++ View/View/EditForm.vb
@@ -0,0 +1,3 @@
+Public Class EditForm
+
+End Class
