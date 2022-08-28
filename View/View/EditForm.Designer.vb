910a4d1c4652ceda7a4137d1f4d1c71be377b1aa
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-08-28 13:57:58 +0900

    フォームをデザイン
    (1) コンポーネントを配置

--- View/View/EditForm.Designer.vb
+++ View/View/EditForm.Designer.vb
@@ -30,7 +30,52 @@ Partial Class EditForm
         CType(Me.dgvRecord, System.ComponentModel.ISupportInitialize).BeginInit
         Me.SuspendLayout
 
+        '
+        'btnOK
+        '
+        resources.ApplyResources(Me.btnOK, "btnOK")
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.UseVisualStyleBackColor = true
+
+        '
+        'btnApply
+        '
+        resources.ApplyResources(Me.btnApply, "btnApply")
+        Me.btnApply.Name = "btnApply"
+        Me.btnApply.UseVisualStyleBackColor = true
+
+        '
+        'btnCancel
+        '
+        resources.ApplyResources(Me.btnCancel, "btnCancel")
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.UseVisualStyleBackColor = true
+
+        '
+        'dgvRecord
+        '
+        Me.dgvRecord.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        resources.ApplyResources(Me.dgvRecord, "dgvRecord")
+        Me.dgvRecord.Name = "dgvRecord"
+        Me.dgvRecord.RowTemplate.Height = 25
+
+        '
+        'EditForm
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.CancelButton = Me.btnCancel
+
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.btnApply)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.dgvRecord)
+        Me.Controls.Add(Me.fraInput)
+        Me.Controls.Add(Me.fraView)
+
         Me.Name = "EditForm"
+        Me.fraView.ResumeLayout(false)
+
         CType(Me.dgvRecord, System.ComponentModel.ISupportInitialize).EndInit
         Me.ResumeLayout(false)
     End Sub
