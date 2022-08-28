a56c984d3ec6f6caf7a8ccf8ad7cf1ef41d4164d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-08-28 10:38:45 +0900

    フォームをデザイン
    (1) コンポーネントを配置

--- View/View/EditForm.Designer.vb
+++ View/View/EditForm.Designer.vb
@@ -15,4 +15,31 @@ Partial Class EditForm
 
     Private components As System.ComponentModel.IContainer
 
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = _
+            New System.ComponentModel.ComponentResourceManager(GetType(EditForm))
+
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnApply = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.dgvRecord = New System.Windows.Forms.DataGridView()
+        Me.fraInput = New System.Windows.Forms.GroupBox()
+        Me.fraView = New System.Windows.Forms.GroupBox()
+
+        CType(Me.dgvRecord, System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+
+        Me.Name = "EditForm"
+        CType(Me.dgvRecord, System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+    End Sub
+
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnApply As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents dgvRecord As DataGridView
+    Friend WithEvents fraInput As GroupBox
+    Friend WithEvents fraView As GroupBox
+
 End Class
