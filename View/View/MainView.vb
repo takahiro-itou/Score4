9b242db8acd30269ce85b8016c038340cbe9abf6
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-09-07 22:18:10 +0900

    コンパイルエラーを修正

--- View/View/MainView.vb
+++ View/View/MainView.vb
@@ -253,24 +253,24 @@ Private Sub updateTables(
 
     Me.m_scoreData.countScores(trgLastDate)
     ScoreView.displayScoreTableToGrid(
-            idxLeague, modeMagic, Me.m_scoreData, Me.grdScore)
+            idxLeague, modeMagic, Me.m_scoreData, Me.dgvScore)
 
     Select Case modeExtra
     Case ExtraViewMode.EXTRA_VIEW_MAGIC_NUMBERS
         ScoreView.displayTeamMagicTableToGrid(
                 idxLeague, modeMagic,
-                Me.m_scoreData, Me.grdExtra
+                Me.m_scoreData, Me.dgvExtra
         )
     Case ExtraViewMode.EXTRA_VIEW_WIN_FOR_MATCH
         ScoreView.displayWinsForBeatTableToGrid(
                 idxLeague,
-                Me.m_scoreData, Me.grdExtra
+                Me.m_scoreData, Me.dgvExtra
         )
     Case Else
         ScoreView.displayRestGameTableToGrid(
                 idxLeague, modeSchedule,
                 Score4Wrapper.GameFilter.FILTER_ALL_GAMES,
-                Me.m_scoreData, Me.grdExtra
+                Me.m_scoreData, Me.dgvExtra
         )
     End Select
 
