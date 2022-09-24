4b2ce5222d8472e4d9c639850c8127cbbdbf6436
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-09-24 18:47:37 +0900

    イベントハンドラを実装

--- View/View/LineView.vb
+++ View/View/LineView.vb
@@ -10,13 +10,53 @@ Private m_currentDate As System.DateTime
 ''========================================================================
 Public Sub initializeView(
         ByVal objSource As Score4Wrapper.Document.ScoreDocument,
-        ByVal startupDate As System.DateTime)
+        ByVal startDate As System.DateTime)
 
     If (Me.m_gameScore Is Nothing) Then
         Me.m_gameScore = objSource
     End If
 
     ScoreView.updateLeagueTab(Me.m_gameScore, Me.tabLeague)
+    updateMagicLine(0, startDate)
+
+    ' 指定した日付を選択する。
+    mncDate.SelectionStart = startDate
+
+End Sub
+
+''========================================================================
+''    優勝／プレーオフ進出ラインを表示する。
+''========================================================================
+Private Sub updateMagicLine(
+        ByVal idxLeague As Integer,
+        Byval targetDate As System.DateTime)
+
+    With Me
+        .m_currentLeague = idxLeague
+        .m_currentDate = targetDate
+    End With
+    lblDate.Text = targetDate
+
+End Sub
+
+''========================================================================
+''    カレンダーをクリックしたら、その日付までのデータに切り替える。
+''========================================================================
+Private Sub mncDate_DateChanged(sender As Object, e As DateRangeEventArgs) _
+        Handles mncDate.DateChanged
+
+    updateMagicLine(Me.m_currentLeague, mncDate.SelectionStart)
+
+End Sub
+
+''========================================================================
+''    タブを選択したら、そのリーグの情報に切り替える。
+''========================================================================
+Private Sub tabLeague_SelectedIndexChanged(sender As Object, e As EventArgs) _
+        Handles tabLeague.SelectedIndexChanged
+
+    Dim idxLeague As Integer = tabLeague.SelectedIndex
+    updateMagicLine(idxLeague, Me.m_currentDate)
 
 End Sub
 
