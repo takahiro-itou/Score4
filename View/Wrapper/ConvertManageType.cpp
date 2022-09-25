a448f4f976dd0340b16fedfa92eba3348564d5bc
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-09-25 19:39:04 +0900

    ソースファイルを新規作成

---
+++ View/Wrapper/ConvertManageType.cpp
@@ -0,0 +1,34 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Score4 Wrapper  Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      マネージ型とアンマネージ型の変換。
+**
+**      @file       ConvertManageType.cpp
+**/
+
+#include    "StdAfx.h"
+
+#include    "ConvertManageType.h"
+
+#include    "ScoreInterface.h"
+
+namespace  Score4Wrapper  {
+
+//========================================================================
+//
+//    Support Functions.
+//
+
+}   //  End of namespace  Score4Wrapper
---
+++ View/Wrapper/ConvertManageType.h
@@ -0,0 +1,32 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Score4 Wrapper  Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2022, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      マネージ型とアンマネージ型の変換。
+**
+**      @file       ConvertManageType.h
+**/
+
+#pragma     once
+
+#include    "Score4Types.h"
+
+namespace  Score4Wrapper  {
+
+//========================================================================
+//
+//    Support Functions.
+//
+
+}   //  End of namespace  Score4Wrapper
