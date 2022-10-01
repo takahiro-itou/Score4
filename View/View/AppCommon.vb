2d554e6decbc1fdade99d1c4716aa421805a9a2d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-10-01 17:34:02 +0900

    フォームの初期位置計算を修正
    (1) オーナーフォームがある場合は画面中央ではなくオーナーの中央とする

--- View/View/AppCommon.vb
+++ View/View/AppCommon.vb
@@ -10,8 +10,10 @@ Public Sub moveWindowToStartPosition(
         ByRef ownerForm As System.Windows.Forms.Form)
 
     Dim fx As Integer, fy As Integer, fw As Integer, fh As Integer
+    Dim bx As Integer, by As Integer, bw As Integer, bh As Integer
     Dim sc As System.Windows.Forms.Screen
-    Dim sx As Integer, sy As Integer
+    Dim sx As Integer, sy As Integer, sw As Integer, sh As Integer
+    Dim sr As Integer, sb As Integer
 
     With targetForm
         fw = .Width
@@ -22,17 +24,34 @@ Public Sub moveWindowToStartPosition(
     With sc.Bounds
         sx = .Left
         sy = .Top
+        sw = .Width
+        sh = .Height
+        sr = .Right
+        sb = .Bottom
     End With
+    If (ownerForm Is Nothing) Then
+        bx = sx
+        by = sy
+        bw = sw
+        bh = sh
+    Else
+        With ownerForm
+            bx = .Left
+            by = .Right
+            bw = .Width
+            bh = .Height
+        End With
+    End If
 
     fx = GetSettingINI(iniFileName, iniSecName, "Left", -1)
     fy = GetSettingINI(iniFileName, iniSecName, "Top", -1)
-    If (fx < sx) Or (fx + fw > sc.Bounds.Right) Then
-        ' ウィンドウが画面からはみ出す場合は、画面中央に移動させる。 '
-        fx = sx + (sc.Bounds.Width - fw) \ 2
+    If (fx < sx) Or (fx + fw > sr) Then
+        ' ウィンドウが画面からはみ出す場合は、オーナー中央に移動させる。 '
+        fx = bx + (bw - fw) \ 2
     End If
-    If (fy < sy) Or (fy + fh > sc.Bounds.Bottom) Then
-        ' ウィンドウが画面からはみ出す場合は、画面中央に移動させる。 '
-        fy = sy + (sc.Bounds.Height - fh) \ 2
+    If (fy < sy) Or (fy + fh > sb) Then
+        ' ウィンドウが画面からはみ出す場合は、オーナー中央に移動させる。 '
+        fy = by + (bh - fh) \ 2
     End If
 
     targetForm.Bounds = New Rectangle(fx, fy, fw, fh)
